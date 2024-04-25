5f8af04fed311b775d6463ea404aaa2ee9a73382
seoAreum <1971236@hansung.ac.kr>
2024-04-25 18:29:53 +0900

    feat: WritingMarkup Challenge

--- Arooming/src/week_1/page/Week_1.tsx
+++ Arooming/src/week_1/page/Week_1.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FirstComponent from "./FirstComponent";
 import ImportingAndExporting from "./ImportingAndExporting";
+import WritingMarkup from "../components/WritingMarkup";
 
 const Week_1 = () => {
   return (
     <React.Fragment>
       <FirstComponent />
       <ImportingAndExporting />
+      <WritingMarkup />
     </React.Fragment>
   );
 };
