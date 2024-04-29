af9aa2ec4cf1101bc6db26e8029115c639bab2d4
Yeonseo-Jo <jys073190@gmail.com>
2024-04-30 03:58:46 +0900

    tic-tac-toe : Making an interactive component

--- Yeonseo/tic-tac-toe/src/App.js
+++ Yeonseo/tic-tac-toe/src/App.js
@@ -1,11 +1,28 @@
 //App.js의 코드는 컴포넌트를 생성한다.
 //컴포넌트 - 사용자 인터페이스 일부를 표시하는 재사용 가능한 코드 조각으로, UI를 렌더링, 관리, 업데이트한다.
 
+import { useState } from "react";
+
 //Saqure 컴포넌트를 만들어, 재사용하기 -> ❕컴포넌트는 대문자로 시작해야 한다
 //props로 각 사각형이 가져야 할 값(value) 부모->자식 컴포넌트로 전달하기
 //❕jsx에서 js로 escape하려면, 중괄호 내에 js 변수를 써줘야 한다.
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  // 값을 기억하기 위해, useState라는 특별한 함수 사용
+  // ❕컴포넌트를 재사용해도, 각 컴포넌트 내에는 고유한 state가 있어 다른 컴포넌트와 독립적임
+  const [value, setValue] = useState(null);
+
+  //사용자와 상호작용하는 이벤트 핸들러 함수 정의
+  function handleClick() {
+    // 클릭 시 value를 x로 바꾸기
+    // ❕onClick 핸들러에서 set 함수 호출되면, 해당 state가 변경 된 컴포넌트(===Square)를 재렌더링한다.
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 //✨export 키워드 - 파일 외부에서 접근 가능
@@ -19,19 +36,19 @@ export default function Board() {
     //React 컴포넌트는 단일 JSX element를 반환해야 한다. -> 이런 경우 Fragments로 감싸야 한다.
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   ); //버튼을 반환한다.
